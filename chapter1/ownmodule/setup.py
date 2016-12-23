b461297ed4add08825165135b8ee993342f6f390
Todd Leonhardt <tleonhardt@sigovs.com>
2016-12-23 00:02:10 -0500

    Fixed Makefile and setup.py for PyAddFunction example

--- chapter1/ownmodule/setup.py
+++ chapter1/ownmodule/setup.py
@@ -1,7 +1,12 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+from distutils.extension import Extension
+
+sourcefiles = ['PyAddFunction.pyx', 'AddFunction.c']
+
+extensions = [Extension("PyAddFunction", sourcefiles)]
 
 setup(
     name = 'PyAddFunction',
-    ext_modules = cythonize('PyAddFunction.pyx', compiler_directives={'embedsignature': True}),
+    ext_modules = cythonize(extensions, compiler_directives={'embedsignature': True}),
 )
